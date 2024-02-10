9fcdc2fecbab7d923a30c72dd066bd3266152166
Ireneusz Cisło <106574096+irekc@users.noreply.github.com>
2024-02-10 08:30:06 +0100

    dark mode at itemListDescription added (#1)

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -11,12 +11,12 @@ export const ProductListItemDescription = ({
 	return (
 		<div className="mt-2 flex justify-between p-2">
 			<div>
-				<h3 className=" text-sm font-semibold text-gray-700">{name}</h3>
-				<p className="text-sm text-gray-500">
+				<h3 className=" text-sm font-semibold text-gray-700 dark:text-gray-200">{name}</h3>
+				<p className="text-sm text-gray-500 dark:text-gray-300">
 					<span className="sr-only">Kategoria:</span> {category}
 				</p>
 			</div>
-			<p className="text-sm font-semibold text-gray-900">
+			<p className="text-sm font-semibold text-gray-900 dark:text-gray-100">
 				<span className="sr-only">Cena:</span> {formatMoney(price)}
 			</p>
 		</div>
