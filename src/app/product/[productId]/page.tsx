7692fb42e6b4e6ca0501a07061275e5ee4e131a5
Ireneusz Cisło <irekc86@o2.pl>
2024-02-20 09:03:41 +0100

    singleProductPage suggestedProducts className added

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,28 +1,31 @@
 import { Suspense } from "react";
-import {type  Metadata } from "next";
-import { getProductById} from "@/api/products";
+import { type Metadata } from "next";
+import { getProductById } from "@/api/products";
 import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescription";
 import { ProductCoverImage } from "@/ui/atoms/ProductListItemImage";
 import { SuggestedProductsList } from "@/ui/organisms/SuggestedProductsList";
-
 
 // export const generateStaticParams = async () => {
 // 	const products = await getProdutsList();
 // 	return products.map((product) => ({ productId: product.id }));
 // };
 
-export const generateMetadata = async ({params} : {params: {productId: string}}) : Promise<Metadata> => {
-	const product = await getProductById(params.productId)
+export const generateMetadata = async ({
+	params,
+}: {
+	params: { productId: string };
+}): Promise<Metadata> => {
+	const product = await getProductById(params.productId);
 	return {
 		title: `${product.name} -Sklep internetowy`,
 		description: product.description,
 		openGraph: {
-			title: `${product.name} -Sklep internetowy`, 
+			title: `${product.name} -Sklep internetowy`,
 			description: product.description,
-			images: [product.coverImage.src]
-		}
-	}
-}
+			images: [product.coverImage.src],
+		},
+	};
+};
 
 // export const metadata: Metadata = {
 // 	title: 'Product 123'
@@ -33,17 +36,16 @@ export default async function SingleProductPage({ params }: { params: { productI
 
 	return (
 		<>
-			<article className="mx-auto max-w-md">
+			<article className="mx-auto mt-5 max-w-md px-5">
 				<ProductCoverImage src={product.coverImage.src} alt={product.coverImage.alt} />
 				<ProductListItemDescription product={product} />
 			</article>
-			<aside>
-				<Suspense fallback={"Ładowanie..."}>
+			<aside className="px-5">
+				<h2 className="mx-auto py-10 text-lg font-semibold">Suggested Products</h2>
+				<Suspense aria-busy={true} fallback={"Ładowanie..."}>
 					<SuggestedProductsList />
 				</Suspense>
 			</aside>
 		</>
 	);
 }
-
-
