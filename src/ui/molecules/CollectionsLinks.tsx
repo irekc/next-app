ede6e652b5f2ec694d6acb4996affe4421eef1e3
Ireneusz Cisło <irekc86@o2.pl>
2024-03-07 00:15:47 +0100

    add CollectionsLinks to Navbar

---
+++ src/ui/molecules/CollectionsLinks.tsx
@@ -0,0 +1,22 @@
+import { getCollectionsList } from "@/api/products";
+import { ActiveLink } from "@/ui/atoms/ActiveLink";
+
+export async function CollectionsLinks() {
+	const collections = await getCollectionsList();
+	return (
+		<>
+			<nav>
+				<h1 className="m-auto text-center font-bold">Collections</h1>
+				<ul className="flex">
+					{collections.map((collection) => (
+						<div key={collection.name} className="px-5">
+							<ActiveLink exact={false} href={`/collections/${collection.slug}/1`}>
+								{collection.name}
+							</ActiveLink>
+						</div>
+					))}
+				</ul>
+			</nav>
+		</>
+	);
+}
--- src/ui/organisms/Navbar.tsx
+++ src/ui/organisms/Navbar.tsx
@@ -1,10 +1,11 @@
 import { ActiveLink } from "@/ui/atoms/ActiveLink";
 import { SearchBox } from "@/ui/atoms/SearchBox";
+import { CollectionsLinks } from "@/ui/molecules/CollectionsLinks";
 
 export const Navbar = () => {
 	return (
 		<nav>
-			<SearchBox placeholder="search..."/>
+			<SearchBox placeholder="search..." />
 			<ul className="mt-4 flex flex-col items-center rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:justify-center md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
 				<ActiveLink exact href="/">
 					Home
@@ -12,9 +13,7 @@ export const Navbar = () => {
 				<ActiveLink exact={false} href="/products">
 					All
 				</ActiveLink>
-				<ActiveLink exact={false} href="/collections/">
-					Collections
-				</ActiveLink>
+				<CollectionsLinks />
 				<ActiveLink exact={false} href="/categories/t-shirts">
 					T-Shirts
 				</ActiveLink>
